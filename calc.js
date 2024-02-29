68ffdd9176d2223e17bd3f05d5ef6488ae4ba273
Sudheshna-P <123935356+Sudheshna-P@users.noreply.github.com>
2024-02-29 19:23:55 +0530

    new commit

---
+++ calc.js
@@ -0,0 +1,14 @@
+
+function clr() {
+    document.getElementById("result").value = "";
+}
+ 
+function display(value) {
+    document.getElementById("result").value += value;
+}
+ 
+function calc() {
+    var p = document.getElementById("result").value;
+    var q = eval(p);
+    document.getElementById("result").value = q;
+}
